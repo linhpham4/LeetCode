a6d0074a02d41472d3c3146a1b6be20ebad2b556
Linh Pham <linh.p44@gmail.com>
2025-02-05 01:48:43 -0600

    Time: 27 ms (5.59%), Space: 64.5 MB (5.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -27,6 +27,14 @@ compare letters with one pointer starting from the beginning and the other at th
 
 Time: O(n) >>> left pointer iterates through 1st half of string and right iterates through 2nd half, effectively iterating through all n characters of string
 Space: O(1) >>> toLowerCase() is called on individual characters of the string so it will not create new large data structures (temporary transformation applied to each character during the comparison)
+
+Recursion-------
+Compare 1st and last letters, if different then return false
+If they are the same, call the function but with those 2 letters taken out until there is 1 or less character in the string
+
+Time: O(n^2) >>> slice operation is performed with each recursion O(n) and call the function recursively O(n)
+Space: O(n) >>> maximum number of recursive calls is O(n), each call creates a new substring (via slice) of at most O(n) space, but space for substring is used temporarily for each recursive call so you never hold onto more than the current substring in memory at any one time
+
  */
 
 // var isPalindrome = function (s) {
@@ -35,26 +43,79 @@ Space: O(1) >>> toLowerCase() is called on individual characters of the string s
 //     return (cleanedS === cleanedS.split("").reverse().join(""));
 // };
 
-var isPalindrome = function (s) {
-    let left = 0;
-    let right = s.length - 1;
-    const regex = new RegExp(/[\W_]/);
+// var isPalindrome = function (s) {
+//     let left = 0;
+//     let right = s.length - 1;
+//     const regex = new RegExp(/[\W_]/);
 
-    while (left < right) {
-        while (left < right && regex.test(s[left])) {
-            left++;
-        }
+//     while (left < right) {
+//         while (left < right && regex.test(s[left])) {
+//             left++;
+//         }
 
-        while (left < right && regex.test(s[right])) {
-            right--;
-        }
+//         while (left < right && regex.test(s[right])) {
+//             right--;
+//         }
 
-        if (left < right && s[left].toLowerCase() !== s[right].toLowerCase()) {
-            return false;
-        }
+//         if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+//             return false;
+//         }
 
-        left++;
-        right--;
+//         left++;
+//         right--;
+//     }
+//     return true;
+// }
+
+var isNotAlphanumeric = function (s) {
+    const regex = new RegExp (/[\W_]/)
+    return regex.test(s);
+}
+
+// var isPalindrome = function (s) {
+//     if (s.length <= 1) {
+//         return true;
+//     }
+
+//     let start = s[0];
+//     let end = s.slice(-1);
+        
+//     if (isNotAlphanumeric(start)) {
+//         return isPalindrome(s.slice(1))
+//     } else if (isNotAlphanumeric(end)) {
+//         return isPalindrome(s.slice(0, -1))
+//     }
+
+//     if (start.toLowerCase() === end.toLowerCase()) {
+//         return isPalindrome(s.slice(1, -1));
+//     }
+
+//     return false;
+// }
+
+/*
+Recusion with 2 pointers where space is O(n)
+*/
+var isPalindrome = function (s, left = 0, right = null) {
+    if (right === null) {
+        right = s.length - 1;
     }
-    return true;
+
+    if (left >= right) {
+        return true;
+    }
+
+    if (isNotAlphanumeric(s[left])) {
+        return isPalindrome(s, left + 1, right);
+    }
+
+    if (isNotAlphanumeric(s[right])) {
+        return isPalindrome(s, left, right - 1);
+    }
+
+    if (s[left].toLowerCase() === s[right].toLowerCase()) {
+        return isPalindrome(s, left + 1, right - 1);
+    }
+
+    return false;
 }
