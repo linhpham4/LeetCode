32679e617083357575fbcd6d64c26116841273c6
Linh Pham <linh.p44@gmail.com>
2025-02-24 02:00:07 -0600

    Time: 3 ms (13.14%), Space: 55.8 MB (9.66%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -6,10 +6,39 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
+ /*
+ Input: arr (nodes of binary tree)
+Output: int (depth of tree)
+
+1) Complete tree: root = [1,2,3] // 3
+2) Tree with null nodes: root = [1,2,3,null,null,4] // 3
+3) One node: root = [1] // 1
+4) No nodes: root = [] // 0
+
+DFS------------
+Recursively add the max depth of left and right subtree to the current node level
+
+Time: O(n) >>> number of nodes
+Space: O(h) >>> height of tree
+    Balanced tree = O(logn)
+    Degenerate tree = O(n)
+
+BFS----------
+Traverse tree level by level. Initialize a queue with root & create variable for current node and depth.
+For the current node being removed from the queue, add its children into queue.
+If there node has children, increase depth counter.
+
+
+Time: O(n)
+Space: O(n)
+ */
+
 /**
  * @param {TreeNode} root
  * @return {number}
  */
+ /*
 var maxDepth = function(root) {
     if (root === null) {
         return 0;
@@ -17,3 +46,29 @@ var maxDepth = function(root) {
 
     return (1 + Math.max(maxDepth(root.left), maxDepth(root.right)));
 };
+*/
+var maxDepth = function(root) {
+    const queue = [];
+    let depth = 0;
+
+    if (root !== null) {
+        queue.push(root);
+    }
+
+    while(queue.length > 0) {
+        const currentLength = queue.length;
+        for (let i = 0; i < currentLength; i++) {
+            const currentNode = queue.shift();
+
+            if(currentNode.left !== null) {
+                queue.push(currentNode.left);
+            }
+
+            if(currentNode.right !== null) {
+                queue.push(currentNode.right);
+            }
+        }
+        depth++;
+    }
+    return depth;
+}
