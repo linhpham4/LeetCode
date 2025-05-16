01d10c409f588d09d3ff8e3228901bdbd77c3ebd
Linh Pham <linh.p44@gmail.com>
2025-05-16 00:56:43 -0500

    Time: 5 ms (25.04%), Space: 54.1 MB (92.68%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,70 +3,26 @@
  * @return {boolean}
  */
 
- /*
-    Input: string of bracket chars
-    Output: Boolean (true if valid, false if not valid)
-
-    Valid string:
-    Opening bracket must be closed with the same type
-    Correct order
-    Close brackets must be paired with corresponding opening bracket
-
-    1. One pair: "[]" //true
-    2. Multiple types: "()[]" //true
-    3. Nested brackets: "([])" //true
-    4. Nested, but wrong order: "([)]" //false
-    5. No close bracket: "(" //false
-    6. No opening bracket: ")" //false
-    7. Different types: "(]" //false
-    8. Empty string: "" //false
-
-    Brute Force-----------
-    While valid bracket pairs remain, replace them with empty string
-    
-    Time: O(n^x)
-    Space: O(n)
-
-    Stack---------
-    Create empty array
-    Create hash table {closing bracket: opening bracket}
-    Iterate through string, pushing open brackets into array
-    If the character is a closing bracket
-        If the current closing bracket is equal to last element of array, pop
-        Otherwise return false
-    After iteration: if array is empty, return true. Otherwise return false
-
-    Time: O(n)
-    Space: O(n)
-*/
-
-// var isValid = function(s) {
-//     while (s.includes("()") || s.includes("[]") || s.includes("{}")) {
-//         s = s.replace("()", "").replace("[]", "").replace("{}", "");
-//     }
-
-//     return s === "";
-// };
-
 var isValid = function(s) {
     let stack = [];
-    const brackets = {
-        ")": "(",
-        "]": "[",
-        "}": "{"
+    const types = {
+        ')': '(',
+        '}': '{',
+        ']': '['
     }
 
     for (let i = 0; i < s.length; i++) {
-        if (brackets.hasOwnProperty([s[i]])) {
-            if (stack[stack.length-1] === brackets[s[i]]) {
+        const currBracket = s[i];
+        if (types[currBracket]) {
+            if (types[currBracket] === stack[stack.length-1]) {
                 stack.pop();
             } else {
                 return false;
             }
         } else {
-            stack.push(s[i]);
+            stack.push(currBracket);
         }
     }
 
     return stack.length === 0;
-}
+};
